2f807e04aaf6a3a4cee26e28d491c1aec4b0b6cd
Roderic Page <rdmpage@gmail.com>
2020-07-10 16:22:17 +0100

    First commit

---
+++ config.inc.php
@@ -0,0 +1,83 @@
+<?php
+
+
+global $config;
+
+// Date timezone--------------------------------------------------------------------------
+date_default_timezone_set('UTC');
+
+// Multibyte strings----------------------------------------------------------------------
+mb_internal_encoding("UTF-8");
+
+// Hosting--------------------------------------------------------------------------------
+
+$site = 'local';
+$site = 'heroku';
+
+switch ($site)
+{
+	case 'heroku':
+		// Server-------------------------------------------------------------------------
+		$config['web_server']	= 'https://pid-demonstrator.herokuapp.com'; 
+		$config['site_name']	= 'PID Demonstrator';
+
+		// Files--------------------------------------------------------------------------
+		$config['web_dir']		= dirname(__FILE__);
+		$config['web_root']		= '/';		
+		break;
+
+	case 'local':
+	default:
+		// Server-------------------------------------------------------------------------
+		$config['web_server']	= 'http://localhost'; 
+		$config['site_name']	= 'BibFinder';
+
+		// Files--------------------------------------------------------------------------
+		$config['web_dir']		= dirname(__FILE__);
+		$config['web_root']		= '/~rpage/bibfinder-o/';
+		break;
+}
+
+// Environment----------------------------------------------------------------------------
+// In development this is a PHP file that is in .gitignore, when deployed these parameters
+// will be set on the server
+if (file_exists(dirname(__FILE__) . '/env.php'))
+{
+	include 'env.php';
+}
+
+$config['cache']					= dirname(__FILE__) . '/cache';
+
+
+$config['platform'] = 'local';
+$config['platform'] = 'cloud';
+
+if ($config['platform'] == 'local')
+{
+
+	// Local Docker Elasticsearch version 7.6.2 http://localhost:32772
+	$config['elastic_options'] = array(
+			'protocol' => 'http',
+			'index' => 'bibfinder',
+			'protocol' => 'http',
+			'host' => 'localhost',
+			'port' => 32769
+			);
+
+}
+
+if ($config['platform'] == 'cloud')
+{
+
+	// Bitnami
+	$config['elastic_options'] = array(
+			'index' 	=> 'elasticsearch/bibfinder',
+			'protocol' 	=> 'http',
+			'host' 		=> '35.204.73.93',
+			'port' 		=> 80,
+			'user' 		=> getenv('ELASTIC_USERNAME'),
+			'password' 	=> getenv('ELASTIC_PASSWORD'),
+			);
+}
+
+?>
---
+++ search.php
@@ -0,0 +1,362 @@
+<?php
+
+// Elastic search
+
+error_reporting(E_ALL);
+
+require_once(dirname(__FILE__) . '/config.inc.php');
+require_once(dirname(__FILE__) . '/elastic.php');
+
+//----------------------------------------------------------------------------------------
+
+function do_search($q, $limit = 5)
+{
+	global $elastic;
+
+	$json = '{
+	"size":10,
+		"query": {
+		   "multi_match" : {
+		  "query": "<QUERY>",
+		  "fields":["search_data.fulltext", "search_data.fulltext_boosted^4"] 
+		}
+	},
+
+	"highlight": {
+		  "pre_tags": [
+			 "<mark>"
+		  ],
+		  "post_tags": [
+			 "<\/mark>"
+		  ],
+		  "fields": {
+			 "search_data.fulltext": {},
+			 "search_data.fulltext_boosted": {}
+		  }
+	   },
+
+	"aggs": {
+
+
+	"by_cluster_id": {
+			"terms": {
+				"field": "search_data.cluster_id.keyword",
+				"order": {
+					"max_score.value": "desc"
+				}
+			},
+	
+	
+			"aggs": {
+				"max_score": {
+					"max": {
+						"script": {
+							"lang": "painless",
+							"inline": "_score"
+						}
+					}
+				}
+			}
+		}
+	 }
+
+	}';
+
+	$json = str_replace('<QUERY>', $q, $json);
+	//$json = str_replace('<SIZE>', $limit, $json);
+
+	$response = $elastic->send('POST',  '_search?pretty', $json);					
+
+	// echo $response;
+
+	$obj = json_decode($response);
+
+	// process and convert to RDF
+
+	// schema.org DataFeed
+	$output = new stdclass;
+
+	$output->{'@context'} = (object)array
+		(
+			'@vocab'	 => 'http://schema.org/',
+			'bibo' 		=> 'http://purl.org/ontology/bibo/',
+			'doi' => 'bibo:doi',
+			'handle' => 'bibo:handle',
+			'sici'	=> 'bibo:sici',
+			'dcterms' => 'http://purl.org/dc/terms/',
+			'bibliographicCitation' => 'dcterms:bibliographicCitation'
+		);
+
+	$output->{'@graph'} = array();
+	$output->{'@graph'}[0] = new stdclass;
+	$output->{'@graph'}[0]->{'@id'} = "http://example.rss";
+	$output->{'@graph'}[0]->{'@type'} = "DataFeed";
+	$output->{'@graph'}[0]->dataFeedElement = array();
+
+	$clusters = array();
+
+	if (isset($obj->hits))
+	{
+		$num_hits = 0;
+		
+		// Elastic 7.6.2
+		if (isset($obj->hits->total->value))
+		{
+			$num_hits = $obj->hits->total->value;
+		}
+		else
+		{
+			$num_hits = $obj->hits->total;			
+		}
+	
+		if ($num_hits > 0)
+		{
+			// Get list of clusters				
+			foreach ($obj->aggregations->by_cluster_id->buckets as $bucket)
+			{
+				$clusters[$bucket->key] = array();
+			}
+		
+			// Cluster results
+			foreach ($obj->hits->hits as $hit)
+			{
+				$cluster_id = $hit->_source->search_data->cluster_id;
+			
+				if (!isset($clusters[$cluster_id]))
+				{
+					$clusters[$cluster_id] = array();
+				}
+			
+				$clusters[$cluster_id][$hit->_source->id] = $hit->_source->search_display;
+			}
+	
+		}
+
+	}
+
+	// simplest approach, just use details from first element in cluster 
+	foreach ($clusters as $cluster_id => $cluster)
+	{
+		if (count($cluster))
+		{
+	
+			$item = new stdclass;
+			$item->{'@id'} = $cluster_id;
+			$item->{'@type'} = array("DataFeedItem", "ItemList");
+			
+			// Array is indexed by cluster_id, so we need to get first element (can't use "0")
+			reset($cluster);
+			$first_cluster_item_index = key($cluster);
+	
+			$item->name = $cluster[$first_cluster_item_index]->name;
+			
+			if (isset($cluster[$first_cluster_item_index]->creator))
+			{
+				$item->creator = $cluster[$first_cluster_item_index]->creator;
+			}
+		
+			if (isset($cluster[$first_cluster_item_index]->csl->URL))
+			{
+				$item->url = $cluster[$first_cluster_item_index]->csl->URL;
+			}
+		
+			// make a standard citation for us to check matches against
+			$bibliographicCitation = array();
+		
+			$keys = array('author', 'issued', 'title', 'container-title', 'volume', 'issue', 'page');
+			foreach ($keys as $k)
+			{
+				if (isset($cluster[$first_cluster_item_index]->csl->{$k}))
+				{
+					switch ($k)
+					{
+						case 'title':
+							if (is_array($cluster[$first_cluster_item_index]->csl->{$k}))
+							{
+								$bibliographicCitation[] = ' ' . $cluster[$first_cluster_item_index]->csl->{$k}[0] . '.';
+							}
+							else
+							{
+								$bibliographicCitation[] = ' ' . $cluster[$first_cluster_item_index]->csl->{$k} . '.';						
+							}
+							break;
+
+						case 'container-title':
+							if (is_array($cluster[$first_cluster_item_index]->csl->{$k}))
+							{
+								$bibliographicCitation[] = ' ' . $cluster[$first_cluster_item_index]->csl->{$k}[0];
+							}
+							else
+							{
+								$bibliographicCitation[] = ' ' . $cluster[$first_cluster_item_index]->csl->{$k};						
+							}
+							break;
+
+						case 'author':
+							$authors = array();
+							foreach ($cluster[$first_cluster_item_index]->csl->{$k} as $author)
+							{
+								$author_parts = [];
+								if (isset($author->literal))
+								{
+									$author_parts[] = $author->literal;
+								}
+								else
+								{
+									if (isset($author->given))
+									{
+										$author_parts[] = $author->given;
+									}
+									if (isset($author->family))
+									{
+										$author_parts[] = $author->family;
+									}
+								}
+								$authors[] = join(' ', $author_parts);						
+							}
+							$bibliographicCitation[] = join('; ', $authors);
+							break;
+						
+						case 'issued':
+							if (isset($cluster[$first_cluster_item_index]->csl->{$k}->{'date-parts'}))
+							{
+								$bibliographicCitation[] = ' (' . $cluster[$first_cluster_item_index]->csl->{$k}->{'date-parts'}[0][0] . ').';
+							}					
+							break;
+						
+						case 'page':
+							$bibliographicCitation[] = ': ' . $cluster[$first_cluster_item_index]->csl->{$k};
+							break;
+						
+						case 'issue':
+							$bibliographicCitation[] = '(' . $cluster[$first_cluster_item_index]->csl->{$k} . ')';
+							break;
+				
+						case 'volume':
+							$bibliographicCitation[] = ', ' . $cluster[$first_cluster_item_index]->csl->{$k};
+							break;
+
+						default:
+							$bibliographicCitation[] = $cluster[$first_cluster_item_index]->csl->{$k};
+							break;
+					}
+			
+				}
+		
+			}
+				
+			$item->bibliographicCitation = trim(join('', $bibliographicCitation));
+			
+			if (isset($cluster[$first_cluster_item_index]->csl->abstract))
+			{
+				$item->description = $cluster[$first_cluster_item_index]->csl->abstract;
+			}
+			else
+			{
+				$item->description = $item->bibliographicCitation;
+			}
+		
+			// List of all members	
+			$item->numberOfItems = count($cluster);
+			$item->itemListElement = array();
+	
+			// build list of cluster members, and extract any useful information
+			foreach ($cluster as $id => $cluster_data)
+			{
+				$item->itemListElement[] = $id;
+			
+				if (isset($cluster_data->csl->DOI))
+				{
+					$item->doi = $cluster_data->csl->DOI;
+					
+					if (!isset($item->url))
+					{
+						$item->url = 'https://doi.org/' . $item->doi;
+					}
+				}
+				if (isset($cluster_data->csl->HANDLE))
+				{
+					$item->handle = $cluster_data->csl->HANDLE;
+					
+					if (!isset($item->url))
+					{
+						$item->url = 'https://hdl.handle.net.org/' . $item->handle;
+					}
+					
+				}
+				if (isset($cluster_data->csl->JSTOR))
+				{
+					$item->jstor = $cluster_data->csl->JSTOR;
+				}
+				/*
+				if (isset($cluster_data->csl->SICI))
+				{
+					$item->sici = $cluster_data->csl->SICI;
+				}
+				*/
+				
+				if (isset($cluster_data->csl->WIKIDATA))
+				{
+					$item->sameAs[] = 'http://www.wikidata.org/entity/' . $cluster_data->csl->WIKIDATA;
+				}
+				
+			
+				if (isset($cluster_data->csl->thumbnail))
+				{
+					$item->thumbnailUrl = $cluster_data->csl->thumbnail;
+				}
+			
+				if (isset($cluster_data->csl->link))
+				{
+					foreach ($cluster_data->csl->link as $link)
+					{
+						if ($link->{'content-type'} == 'application/pdf')
+						{
+							if (!isset($item->encoding))
+							{
+								$item->encoding = array();
+							}
+						
+							$encoding = new stdclass;
+							$encoding->url = $link->URL;
+							$encoding->encodingFormat = $link->{'content-type'};
+							$item->encoding[] = $encoding;
+							
+							if (!isset($item->url))
+							{
+								$item->url = $encoding->url;
+							}
+						
+					
+						}
+					}
+				}
+			
+			
+			
+			}
+	
+			$output->{'@graph'}[0]->dataFeedElement[] = $item;
+		}
+	}
+
+	//print_r($output);
+
+	return $output;
+}
+
+
+// test
+if (0)
+{
+	$q = 'Nicolas';
+	
+	$result = do_search($q);
+	
+	print_r($result);
+
+
+}
+
+
+?>
